569076aedf9f32d7de2d02e4df499c01747045c1
Knyazik01 <ndrwkn2001@gmail.com>
2020-09-30 02:34:57 +0300

    Lab_1 and Lab_2

---
+++ Lab_1/script/main.js
@@ -0,0 +1,39 @@
+const
+    value_x = document.querySelector('#value_x'),
+    button = document.querySelector('#calculate'),
+    result = document.querySelector('#result');
+
+
+function calc(x) {
+    return Math.cos(Math.abs(x)) + Math.sqrt(x);
+}
+
+function show_result_in_block(block, x, y) {
+    block.style.display = 'block';
+    value_result = 'Result: ' + (Math.round((y + Number.EPSILON) * 1e5) / 1e5);
+    block.innerText = value_result;
+}
+
+function hide_result_block(block) {
+    block.style.display = 'none';
+    block.innerHTML = '';
+}
+
+function get_result_or_send_alert() {
+    x = Number(value_x.value);
+    y = calc(x);
+    if (value_x.value === '' || isNaN(x) || isNaN(y)) {
+        hide_result_block(result);
+        alert('Please, input correct value of x');
+    } else {
+        show_result_in_block(result, x, y);
+    }
+}
+
+button.addEventListener('click', function () {
+    get_result_or_send_alert();
+});
+
+value_x.addEventListener('input', function () {
+        hide_result_block(result);
+});
---
+++ Lab_2/script/main.js
@@ -0,0 +1,95 @@
+const el_start = document.querySelector('#value_start'),
+    el_stop = document.querySelector('#value_stop'),
+    el_step = document.querySelector('#value_step'),
+    calc_button = document.querySelector('#calculate'),
+    result_content = document.querySelector('#result');
+
+function hideBlock(block) {
+    block.style.display = 'none';
+    block.innerHTML = '';
+}
+
+function getValueAt(x) {
+    return Math.cos(Math.abs(x)) + Math.sqrt(x);
+}
+
+function getIntervalValue(start, stop, step) {
+
+    if (step !== 0) {
+        if (((stop - start) / step) >= 0) {
+
+            const arr = [];
+
+            let value = start;
+            for (let index = 0; index <= (stop - start) / step; index++) {
+                arr.push([value, getValueAt(value) || '&mdash;']);
+                value += step;
+            }
+
+            // if (arr.length !== 0) {
+            return arr;
+            // } else {
+            //     throw new Error('Result is empty. Input correct data!');
+            // }
+
+        } else {
+            throw new Error('Result is empty. Input correct data!');
+        }
+    } else {
+        throw new Error('Step = 0. Input correct data!');
+    }
+
+}
+
+function drawResultTable(array) {
+    let html = '<table class="result_table">' +
+        '<tr>' +
+        '<td><strong>Value of X</strong></td>' +
+        '<td><strong>Result</strong></td>' +
+        '</tr>';
+
+    let x, y;
+    for (let item of array) {
+        x = (Math.round((item[0] + Number.EPSILON) * 1e5) / 1e5);
+        y = (Math.round((item[1] + Number.EPSILON) * 1e5) / 1e5);
+        html += `<tr><td>${x}</td><td>${y}</td></tr>`;
+    }
+
+    html += '</table>'
+    result_content.innerHTML = html;
+    result_content.style.display = 'block';
+    return 0;
+}
+
+function main() {
+    const result = getIntervalValue(
+        +el_start.value,
+        +el_stop.value,
+        +el_step.value);
+
+    drawResultTable(result);
+    return 0;
+}
+
+calc_button.addEventListener('click', function () {
+
+
+    try {
+        main();
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+});
+
+el_start.addEventListener('input', function () {
+    hideBlock(result_content);
+});
+
+el_stop.addEventListener('input', function () {
+    hideBlock(result_content);
+});
+
+el_step.addEventListener('input', function () {
+    hideBlock(result_content);
+});
